1b3f721d86069233833ec0741c4cbfd87cf25ac5
Abinanthan1308 <128885645+Abinanthan1308@users.noreply.github.com>
2024-05-02 19:12:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from PIL import Image
+
+model_id = "vikhyatk/moondream2"
+revision = "2024-04-02"
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, trust_remote_code=True, revision=revision
+)
+tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+
+uploaded_file = st.file_uploader("Upload Image")
+image = Image.open(uploaded_file)
+enc_image = model.encode_image(image)
+
+st.write(model.answer_question(enc_image, "Describe this image.", tokenizer))
